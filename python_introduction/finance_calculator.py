1b7857b02279dda9f495178465b82e49d66c69a5
Akin-ola <idris_akinola@yahoo.com>
2024-12-13 12:27:59 +0100

    modified extra task

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,8 @@
+monthly_income = int(input ("Enter your monthly income "))
+monthly_expenses = int(input ("Enter your monthly expenses "))
+monthly_savings = monthly_income - monthly_expenses
+annual_savings = float(monthly_savings * 12)
+annual_interest = annual_savings * 0.05
+annual_savings_with_interest = annual_savings + annual_interest
+print(monthly_savings)
+print(annual_savings_with_interest)
