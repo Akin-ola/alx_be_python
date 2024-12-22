691637aa4aab4f6caa790b8e88d2212c0d8b094c
Akin-ola <idris_akinola@yahoo.com>
2024-12-22 12:10:50 +0100

    modified multiplication table 1

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
-number = int(input("Enter a number to see it's multiplication table: "))
+number = int(input("Enter a number to see its multiplication table: "))
 for n in range (1, 11):
     result = number * n
     print(f"{number} * {n} = {result}")
