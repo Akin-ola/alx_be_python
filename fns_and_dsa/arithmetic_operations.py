c0a4cfdacad7675754d125a36c014626fb219a3f
Akin-ola <idris_akinola@yahoo.com>
2025-01-09 23:13:59 +0100

    modified script 1

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,8 +6,11 @@ def perform_operation(num1, num2, operation):
     elif operation=='multiply':
         result = num1 * num2
     elif operation=='divide':
-        result = 0 if num2 == 0 else num1 / num2
+        if num2 == 0:
+            result = 0 
+        else:
+            result = num1 / num2
     return result
 
-perform_operation(0, 6, "divide")
+perform_operation(40, 8, "divide")
     
