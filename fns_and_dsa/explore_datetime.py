a83f792ba92522ce1e20c988e4453c821ed47f1a
Akin-ola <idris_akinola@yahoo.com>
2025-01-12 19:11:08 +0100

    modified datetime script

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -5,3 +5,10 @@ def display_current_datetime ():
     print("Current date and time:", current_date)
 
 display_current_datetime()
+def calculate_future_date():
+    current_date = datetime.now()
+    numbers_of_days = int(input("Enter the number of days to add to the current date: "))
+    future_date = current_date + timedelta(numbers_of_days)
+    print(f"Future date: {future_date}")
+    
+calculate_future_date()
