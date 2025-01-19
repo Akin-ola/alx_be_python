f46757606151723fc2b01a023379689e294d0f6e
Akin-ola <idris_akinola@yahoo.com>
2025-01-19 22:23:40 +0100

    Added sctript to library management py file

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -1,7 +1,7 @@
 from robust_division_calculator import safe_divide
 
 numerator = input("Enter numerator: ")
-denominator = input("Enter denominator: ")
+denominator = input("Enter denominator")
 
 result = safe_divide(numerator, denominator)
 print(result)
