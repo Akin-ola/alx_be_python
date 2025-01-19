6fedb3ac6713ecbc0eeea6007368a27f87969cc1
Akin-ola <idris_akinola@yahoo.com>
2025-01-19 19:09:19 +0100

    Added script to  bank account and main-0 py files

---
+++ programming_paradigm/main-0.py
@@ -0,0 +1,23 @@
+from bank_account import BankAccount
+
+account = BankAccount(100)
+
+def main():
+    command = input("Commands: deposit, withdraw, display: ").lower().strip()
+    if command == "deposit":
+        amount = float(input("How much do you want to deposit: "))
+        account.deposit(amount)
+        print(f"Deposited: ${amount}")
+    elif command == "withdraw":
+        amount = float(input("How much do you want to withdraw: "))
+        if account.withdraw(amount):
+            print(f"Withdrew: ${amount}")
+        else:
+            print("Insufficient funds.")
+    elif command == "display":
+        account.display_balance()
+    else:
+        print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
