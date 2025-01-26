6ad24bbeccd99f9a4f8979d7c0e4ddb71131a663
Akin-ola <idris_akinola@yahoo.com>
2025-01-26 19:43:36 +0100

    added script to polymorphism file

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -1,3 +1,39 @@
+import math
+
 class Shape:
     def __init__(self):
-        
+        pass
+    
+    def area(self):
+        raise NotImplementedError
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        super().__init__()
+        self.length = length
+        self.width = width
+
+    def area(self):
+        result = self.length * self.width
+        return result
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__()
+        self.radius = radius
+
+    def area(self):
+        result = math.pi * float(self.radius * self.radius)
+        return result
+
+def main():
+    shapes = [
+        Rectangle(10, 5),
+        Circle(7)
+    ]
+
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+
+if __name__ == "__main__":
+    main()
